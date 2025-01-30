af72413d038192b2fd978b62c9051b351e7607b6
fwrnandaaa <claudiazkn432@gmail.com>
2025-01-30 13:35:01 -0300

    Adicionando primeira plataforma

--- vs1/versao1.py
+++ vs1/versao1.py
@@ -3,7 +3,6 @@ import time
 
 import pygame
 
-#Personagem mexe da esquerda pra direita
 pygame.init()
 width, height = 640, 580
 size = width, height
@@ -12,32 +11,30 @@ pygame.display.set_caption("Doodle jump")
 time = pygame.time.Clock()
 
 imagem_fundo = pygame.image.load("background01.png")
+
 character = pygame.image.load("character_right.png")
-platform = pygame.image.load("platform.png")
 character_rect = character.get_rect()
 character_rect.y = 420
 character_rect.x = 320
-black = (0,0,0)
+
+platform = pygame.image.load("platform.png")
+platform_rect = platform.get_rect()
+platform_rect.center = (width // 2, 578)
 
 character_x = 50
 character_y = 480
-platforms = [(175, 480, 70, 10)]
-blocks = []
-
-for i in range (len(platforms)):
-    block = pygame.draw.rect(tela, black,  platforms[i])
-    blocks.append(block)
 
 while True:
+    tela.blit(platform, platform_rect)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+           pygame.quit()
+           sys.exit()
 
   
     tela.blit(imagem_fundo, (0, 0))
     tela.blit(character, (character_x, character_y))
-    tela.blit(platform, (220, 300))
+    tela.blit(platform, platform_rect)
     pygame.display.flip()
 
 
