a66280b8ab1ba20374985ba3eaa6e5cd841b20af
fwrnandaaa <claudiazkn432@gmail.com>
2025-02-06 17:17:01 -0300

    Simplificando o código anterior, adicionando movimento ao jogador

--- vs1/versao1.py
+++ vs1/versao1.py
@@ -7,62 +7,60 @@ width, height = 400, 500
 size = width, height
 time = pygame.time.Clock()
 fps = 60
-green = (35, 111, 51)
-platforms = [[175, 480, 70, 10]]
-
 
 imagem_fundo = pygame.image.load("background01.png")
 tela = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Doodle jump")
 player = pygame.transform.scale(pygame.image.load("character_right.png"), (90, 70))
+playerrect = player.get_rect()
+playerrect.x = 170
+playerrect.y = 400
 
-player_x = 170
-player_y = 400
-blocks = []
-jump = False
-y_change = player_y
-rect_list= []
+plataforma = pygame.image.load("platform.png")
 
-#atualizando y do jogador
-def player_position(y):
-    global jump
-    global y_change
-    gravity = .4
-    jump_h = 10
-    if jump == True:
-        y_change -= jump_h
-        jump = False
-    y = y_change
-    y_change += gravity
-    return y
-
-#Verificando colisões
-def check_collision(rect_list, j):
-    global player_x
-    global player_y
-    global y_change
-    for i in range(len(rect_list)):
-  
-        if rect_list[i].colliderect([player_x, player_y + 60, 90, 10]) and not jump and y_change > 0:
-            j = True
-    return j
-
+playerrect = pygame.Rect(100, 100, 50, 50)
+is_jumping = False
+jump_height = 0
+jump_speed = -15
+fall_speed = 20
 
 while True:
     time.tick(fps)
-    tela.blit(imagem_fundo, (0, 0))
-    tela.blit(player, (player_x, player_y))
-    for i in range (len(platforms)):
-        block = pygame.draw.rect(tela, green, platforms[i])
-        blocks.append(block)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           pygame.quit()
-           sys.exit()
+            pygame.quit()
+            sys.exit()
 
-    player_y = player_position(player_y)
-    jump = check_collision(blocks, jump)
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_UP] and not is_jumping:
+        is_jumping = True
+        jump_height = 0
 
+    if is_jumping:
+        playerrect.y += jump_speed
+        jump_height += 1
+
+        if jump_height >= 10: 
+            jump_speed = fall_speed  
+        if playerrect.bottom >= 500:  
+            playerrect.bottom = 450
+            is_jumping = False
+            jump_speed = -15
+    if keys[pygame.K_LEFT]:
+        playerrect.x -= 5
+        player = pygame.transform.scale(pygame.image.load("character_left.png"), (90, 70))
+        if playerrect.left < 0:
+            playerrect.left = 0
+    if keys[pygame.K_RIGHT]:
+        playerrect.x += 5
+        player = pygame.transform.scale(pygame.image.load("character_right.png"), (90, 70))
+        if playerrect.right > 500:
+            playerrect.right = 500
+
+
+    tela.blit(imagem_fundo, (0, 0))
+    tela.blit(player, playerrect)
+    tela.blit(plataforma, (width/2, height/2))
     pygame.display.flip()
-
-
