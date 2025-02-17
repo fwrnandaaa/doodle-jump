e5e9be3bf27793f384d68e12664376b69825e81e
fwrnandaaa <claudiazkn432@gmail.com>
2025-02-17 17:07:37 -0300

    colisao

--- vs1/versao1.py
+++ vs1/versao1.py
@@ -2,9 +2,10 @@ import sys
 import time
 import pygame
 import random
+
 pygame.init()
 
-#Constantes gerais
+# Constantes gerais
 width, height = 400, 500
 size = width, height
 time = pygame.time.Clock()
@@ -14,7 +15,7 @@ imagem_fundo = pygame.image.load("background01.png")
 tela = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Doodle jump")
 
-#Constantes referentes as plataformas
+# Constantes referentes as plataformas
 plataforma = pygame.image.load("platform.png")
 altura_plataforma = plataforma.get_height()
 larguraplataforma = plataforma.get_width()
@@ -25,29 +26,37 @@ plataformarect = plataforma.get_rect()
 plataformarect.x = 200
 plataformarect.y = 400
 
-
-#Constantes referentes ao jogador
+# Constantes referentes ao jogador
 player = pygame.transform.scale(pygame.image.load("character_right.png"), (90, 70))
 playerrect = player.get_rect()
 playerrect.x = 200
 playerrect.y = 400
-playerrect = pygame.Rect(200, 400, 0, 50)
 is_jumping = False
 jump_height = 0
 jump_speed = -15
 fall_speed = 20
 
-#Cria e adicionar plataformas
+# Cria e adicionar plataformas
 def criar_plataforma():
     x = random.randint(0, width - larguraplataforma)
-    return {'objeto': plataforma, 'posicao': (x, 0), 'velocidade': (1)}
+    return {'objeto': plataforma, 'posicao': (x, 0), 'velocidade': (3)}
 
-#Função move plataformas
+# Função move plataformas
 def move_plataformas(objeto):
     pos = list(objeto['posicao']) 
     pos[1] += objeto['velocidade'] 
     objeto['posicao'] = tuple(pos) 
-   
+
+# Função para verificar colisão
+def verificar_colisao(playerrect, plataformas):
+    for p in plataformas:
+        plataforma_rect = pygame.Rect(p['posicao'][0], p['posicao'][1], larguraplataforma, altura_plataforma)
+        if playerrect.colliderect(plataforma_rect):
+            if playerrect.bottom <= plataforma_rect.top + jump_speed:  # Verifica se o jogador está caindo
+                playerrect.bottom = plataforma_rect.top   # Ajusta a posição do jogador
+                return True
+
+    return False
 
 while True:
     time.tick(fps)
@@ -55,17 +64,15 @@ while True:
     tela.blit(imagem_fundo, (0, 0))
     tela.blit(player, (playerrect))
 
-    #Criando novas plataformas no topo da tela quando a ultima estiver na metade da tela
+    # Criando novas plataformas no topo da tela quando a ultima estiver na metade da tela
     if len(plataformas) < 1:  
         plataformas.append(criar_plataforma())
-    elif plataformas[-1]['posicao'][1] >= height/2:
+    elif plataformas[-1]['posicao'][1] >= height / 3:
         plataformas.append(criar_plataforma())
 
-  
     for p in plataformas:
         move_plataformas(p)
         tela.blit(p['objeto'], p['posicao']) 
-
 
     plataformas = [p for p in plataformas if p['posicao'][1] < height]
 
@@ -86,10 +93,17 @@ while True:
 
         if jump_height >= 10: 
             jump_speed = fall_speed  
-        if playerrect.bottom >= 500:  
-            playerrect.bottom = 450
+
+        # Verifica colisão com plataformas
+        if verificar_colisao(playerrect, plataformas):
+            is_jumping = False
+            jump_speed = -15  # Reseta a velocidade de pulo
+
+        if playerrect.bottom >= 480:  
+            playerrect.bottom = 480
             is_jumping = False
             jump_speed = -15
+
     if keys[pygame.K_LEFT]:
         playerrect.x -= 5
         player = pygame.transform.scale(pygame.image.load("character_left.png"), (90, 70))
@@ -99,17 +113,7 @@ while True:
         playerrect.x += 5
         player = pygame.transform.scale(pygame.image.load("character_right.png"), (90, 70))
 
-        if playerrect.right > 300:
-            playerrect.right = 300
-            
-    for i in range (len(plataformas)):
-         x = plataformarect[i].x
-         y = plataformarect[i].y
-         crop = pygame.Rect((x,y), (25,25))
-         if not (playerrect.colliderect(crop)):
-             plataformas.append(plataforma[i])
-             plataformarect.append(plataformas[i])
-             
-             ##CORRIGIR ISSO COM BASE NA VERSAO 3 DE JORGIANO
+        if playerrect.right > 400:
+            playerrect.right = 400
 
-         pygame.display.flip()
+    pygame.display.flip()
